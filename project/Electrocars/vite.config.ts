61c4d2c7ecb0f8c1b7da395ab8b0eaad00b95ef9
Alex Listov <sasha.listoff@yandex.ru>
2025-02-13 01:36:17 +0300

    https

--- project/Electrocars/vite.config.ts
+++ project/Electrocars/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+import fs from 'fs';
+import path from 'path';
+
+plugins: [react(), mkcert()]
 
 export default defineConfig({
   plugins: [react()],
   base: "/ElectrocarChargeStation", 
   server: {
+    https:{
+      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
+    },
     port: 3000,
     proxy: {
       "/api": {
